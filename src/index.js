8e7bf786ea70bc1e73f60ebe820957c1652e4123
Admir Mehmedovic <admirmehmedovic58@gmail.com>
2025-04-20 23:02:04 +0200

    refactor(app): fixed hosts

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/wp-2-admir-mehmedovic">
+    <BrowserRouter basename="/wp2-admir-mehmedovic">
     <App />
   </BrowserRouter>
   </React.StrictMode>
